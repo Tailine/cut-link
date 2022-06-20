3a20bc26c315861304cc61500d545dda70db822d
Tailine Chagas <tailine.dejesus@pizzahut.com>
2022-06-19 22:37:57 -0300

    fix: use env port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import linkRoutes from './link/routes'
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
